e85e0d1e85d9c9089ff4713667c4fc36093ef20d
0x2830 <0x2830@tuta.io>
2020-08-29 23:27:44 +0200

    debug

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -22,7 +22,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  echo AAAAA; cat MAkefile; $(MAKE)
+  echo AAAAA; cat Makefile; $(MAKE)
 endef
 
 define $(package)_stage_cmds
